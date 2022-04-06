b0c366a9fcaca79e9fce2f963deed5267efdcddd
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-04-06 15:35:28 +0200

    Initial commit

---
+++ src/test/java/com/example/spring_boot_react_crud/SpringBootReactCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_react_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
